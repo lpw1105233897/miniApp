4dbe8229ceaf1854aed95a7cc71c63a631eb7eb7
lpw <1105233897@qq.com>
2024-09-13 11:39:02 +0800

    优化

--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,16 @@ import react from '@vitejs/plugin-react';
 import commonjs from 'vite-plugin-commonjs';
 
 export default defineConfig(({mode}) => {
-	const env = process.env;
 	return {
 		plugins: [react(), commonjs()],
 		base: mode === 'production' ? '/my-production-base/' : '/',
-		define: {
-			__APP_ENV__: env.VITE_ENV_NAME,
-		},
+
 		resolve: {
 			alias: {
 				'@': '/src',
 			},
 		},
-		server: {
-			proxy: {
-				'/api': {
-					target: env.VITE_API_BASE_URL,
-					changeOrigin: true,
-				},
-			},
-		},
+
 		css: {
 			preprocessorOptions: {
 				less: {
